4158545a9380ff0113a46a0222a830665db3ef76
Steve Bachochin <steve.bachochin@gmail.com>
2019-04-28 10:38:02 -0400

    Calculated API Gravity at 60 degrees F

--- NorthWind/Form1.Designer.cs
+++ NorthWind/Form1.Designer.cs
@@ -32,12 +32,18 @@
             this.buttonSave = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.buttonNewPage = new System.Windows.Forms.Button();
+            this.txtAPIGavity = new System.Windows.Forms.TextBox();
+            this.buttonGetAPIGravity = new System.Windows.Forms.Button();
+            this.txtTemperature = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGravity = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonSelectedValue
             // 
-            this.buttonSelectedValue.Location = new System.Drawing.Point(534, 186);
+            this.buttonSelectedValue.Location = new System.Drawing.Point(256, 186);
             this.buttonSelectedValue.Name = "buttonSelectedValue";
             this.buttonSelectedValue.Size = new System.Drawing.Size(129, 23);
             this.buttonSelectedValue.TabIndex = 1;
@@ -49,7 +55,7 @@
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(328, 186);
+            this.buttonSave.Location = new System.Drawing.Point(146, 186);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(75, 23);
             this.buttonSave.TabIndex = 2;
@@ -69,7 +75,7 @@
             // 
             // buttonNewPage
             // 
-            this.buttonNewPage.Location = new System.Drawing.Point(81, 296);
+            this.buttonNewPage.Location = new System.Drawing.Point(33, 186);
             this.buttonNewPage.Name = "buttonNewPage";
             this.buttonNewPage.Size = new System.Drawing.Size(75, 23);
             this.buttonNewPage.TabIndex = 4;
@@ -77,11 +83,67 @@
             this.buttonNewPage.UseVisualStyleBackColor = true;
             this.buttonNewPage.Click += new System.EventHandler(this.buttonNewPage_Click);
             // 
+            // txtAPIGavity
+            // 
+            this.txtAPIGavity.Location = new System.Drawing.Point(417, 299);
+            this.txtAPIGavity.Name = "txtAPIGavity";
+            this.txtAPIGavity.ReadOnly = true;
+            this.txtAPIGavity.Size = new System.Drawing.Size(100, 20);
+            this.txtAPIGavity.TabIndex = 5;
+            // 
+            // buttonGetAPIGravity
+            // 
+            this.buttonGetAPIGravity.Location = new System.Drawing.Point(325, 299);
+            this.buttonGetAPIGravity.Name = "buttonGetAPIGravity";
+            this.buttonGetAPIGravity.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetAPIGravity.TabIndex = 6;
+            this.buttonGetAPIGravity.Text = "get API Gravity";
+            this.buttonGetAPIGravity.UseVisualStyleBackColor = true;
+            this.buttonGetAPIGravity.Click += new System.EventHandler(this.buttonGetAPIGravity_Click);
+            // 
+            // txtTemperature
+            // 
+            this.txtTemperature.Location = new System.Drawing.Point(208, 302);
+            this.txtTemperature.Name = "txtTemperature";
+            this.txtTemperature.Size = new System.Drawing.Size(100, 20);
+            this.txtTemperature.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Observed Temperature in F";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 339);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Observed Gravity";
+            // 
+            // txtGravity
+            // 
+            this.txtGravity.Location = new System.Drawing.Point(208, 339);
+            this.txtGravity.Name = "txtGravity";
+            this.txtGravity.Size = new System.Drawing.Size(100, 20);
+            this.txtGravity.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtGravity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTemperature);
+            this.Controls.Add(this.buttonGetAPIGravity);
+            this.Controls.Add(this.txtAPIGavity);
             this.Controls.Add(this.buttonNewPage);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonSave);
@@ -92,6 +154,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -102,6 +165,12 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button buttonNewPage;
+        private System.Windows.Forms.TextBox txtAPIGavity;
+        private System.Windows.Forms.Button buttonGetAPIGravity;
+        private System.Windows.Forms.TextBox txtTemperature;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGravity;
     }
 }
 
--- NorthWind/Form1.cs
+++ NorthWind/Form1.cs
@@ -29,6 +29,36 @@ namespace NorthWind
              dataGridView1.DataSource = we.Customers.ToList();
             //string columnName = dataGridView1.CurrentCell.OwningColumn.Name;
             dataGridView1.Columns[3].Visible = false;
+    
+        }
+
+        private double createAPIGravity(int temperature, double obvGravity)
+        {
+            //double obvGravity = gravity; //38.7;
+            int tChange = temperature-60;
+            double hyc;
+            double rho;
+            double rhot;
+            double alfa;
+            double vcf;
+            double rh60 = 0;
+            double api;
+
+            hyc = 1 - (0.00001278 * tChange) - (0.0000000062 * tChange * tChange);
+            rho = 141.5 * 999.012 / (131.5 + obvGravity);
+            rhot = hyc * rho;
+
+            for (int i = 0; i < 9; i++)
+            {
+                alfa = i == 0 ? 341.0957 / rhot / rhot : 341.0957 / rh60 / rh60;
+                vcf = Math.Exp(-alfa * tChange - 0.8 * alfa * alfa * tChange * tChange);
+                rh60 = rhot / vcf;
+            }
+
+            api = Math.Round(141.5 * 999.012 / rh60 - 131.5, 2);
+
+            return api;
+
         }
 
         private void buttonSave_Click(object sender, EventArgs e)
@@ -77,6 +107,17 @@ namespace NorthWind
             // Display the new form.  
             newMDIChild.Dock = DockStyle.Fill;
             newMDIChild.Show();
+        }
+
+        private void buttonGetAPIGravity_Click(object sender, EventArgs e)
+        {
+            int temperature;
+            double gravity;
+
+            temperature = Int32.Parse(txtTemperature.Text);
+            gravity = double.Parse(txtGravity.Text);
+
+            txtAPIGavity.Text = createAPIGravity(temperature, gravity).ToString();
         }
     }
 }
